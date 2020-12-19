a5ef9e997282b2450c0701bf0204b4e452292bb4
Andrew <Andrew@Andrews-MBP.home>
2020-12-19 14:28:51 -0500

    trelloyes assignment 2

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Card.css";
+
+const Card = (props) => {
+  return (
+    <div className="Card">
+      <button type="button" onClick={props.onDelete}>delete</button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  );
+};
+
+export default Card;
